7da9cf75b970b223b7bfb7737f7f61e0d1e9b47b
huy2272 <huy2272@gmail.com>
2022-10-11 19:33:22 -0400

    declaring and using variables in C

--- hello.c
+++ hello.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
-void main(){
-    printf("Hello World!");
+int main(){
+    printf("Hello World!\n");
+    int a = 100;
+    if  (a<20){
+        printf("a is less than 20\n");
+    }
+    else{
+        printf("a is not less than 20\n");
+    }
+    printf("value of a is : %d\n", a);
+
+    return 0;
 }
