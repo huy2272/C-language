25b7528ebb7b979d0cef2510a2472f7bf2d8e8f0
huy2272 <huy2272@gmail.com>
2022-10-25 17:27:57 -0400

    reading string from file, and reading formatted input from a file

--- fileIO/readingFile.c
+++ fileIO/readingFile.c
@@ -18,5 +18,34 @@ int main(){
     }
     fclose(fp);
     fp = NULL;
+
+    //Reading a string
+    //We need to allocate memory space for this pointer
+    char str[61];
+    fp = fopen("apple.txt", "r");
+
+    if (fp == NULL)
+    {
+        perror("Error opening file");
+        return(-1);
+    }
+    
+    while (fgets(str,60,fp) != NULL)
+    {
+        printf("%s", str);
+    }
+    fclose(fp);
+    fp = NULL;
+
+    //Reading formatted input from a file
+    fp = fopen("apple.txt", "r");
+    char str1[10], str2[10], str3[10];
+    fscanf(fp, "%s %s %s", str1, str2, str3);
+    printf("\nstr1: |%s|\n", str1);
+    printf("str2: |%s|\n", str2);
+    printf("str3: |%s|\n", str3);
+    fclose(fp);
+    fp = NULL;
+
     return 0;
 }
