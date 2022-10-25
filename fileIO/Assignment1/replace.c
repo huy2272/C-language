61d60656f6ed502236f2cc8a10b34ba7663515cb
huy2272 <huy2272@gmail.com>
2022-10-25 19:43:29 -0400

    added check to make sure user entered at least 1 string and not more than that

--- fileIO/Assignment1/replace.c
+++ fileIO/Assignment1/replace.c
@@ -4,10 +4,18 @@
 
 int main(int argc, char const *argv[])
 {
+    if (argc != 2)
+    {
+        printf("Please make sure to enter one string\n");
+        return 0;
+    }
+    
     int strSize = 0;
-    strSize += strlen(argv[1]);
-    printf("strSize = %d\n", strSize);
     char *strUser;
+
+    //Find out the length of user's input string
+    strSize += strlen(argv[1]);
+    //Allocate memory for user's input string
     strUser = malloc(strSize);
     strcat(strUser, argv[1]);
     printf("cmmd string is: %s\n", strUser); 
