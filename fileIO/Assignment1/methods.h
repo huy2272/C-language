10bdeef7303b2b890620643550d8bdd9fcf8c2e5
huy2272 <huy2272@gmail.com>
2022-10-26 13:49:42 -0400

    Implemented a method can can read files from the current directory, this method can also distinguish files from directory.

--- fileIO/Assignment1/methods.h
+++ fileIO/Assignment1/methods.h
@@ -1,3 +1,4 @@
 int Srch(char *fname, char *str);
 void CurrFile(char *fname);
 void Report(int hits, char *fname, char *str);
+int Traverse();
--- fileIO/Assignment1/traversal.c
+++ fileIO/Assignment1/traversal.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
-void CurrFile(char *fname){
-    printf("Currently in %s\n", fname);
+int Traverse(){
+    struct dirent *entry;
+    struct stat filestat;
+    DIR *dir = opendir(".");
+    if (dir == NULL)
+    {
+        printf("Could not open current directory");
+        return 0;
+    }
+    
+    
+    while ((entry = readdir(dir)) != NULL){
+        stat(entry->d_name,&filestat);
+        if (S_ISDIR(filestat.st_mode))
+            printf("Directory: %s\n", entry->d_name);
+        else
+            printf("File: %s\n", entry->d_name);
+    }
+    closedir(dir);
+    return 0;
 }
